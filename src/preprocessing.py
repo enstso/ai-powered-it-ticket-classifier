e2b9b7eff3f355c46fbe8b5f3c7a75da15195850
Enstso <enstso@outlook.fr>
2026-07-22 00:15:58 +0200

    preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,7 +1,40 @@
+import re
+import spacy
+import spacy.lang
 def preprocess_text(text:str)->str:
-    lower_text = text.lower()
-    tokenization = lower_text.split()
-    print(tokenization)
+    tokens = tokenization(text)
+    print(tokens)
+    tokens = remove_stop_words(tokens)
+    print(tokens)
+    tokens = lemma_words(tokens)
+    print(tokens)
+
+def remove_stop_words(tokens:list[str])->list[str]:
+    text = " ".join(tokens)
+    nlp = spacy.blank("fr")
+    doc = nlp(text)
+
+    new_tokens = [
+        token.text
+        for token in doc
+        if not token.is_stop
+    ]
+
+    return new_tokens
+
+def lemma_words(tokens:list[str])->list[str]:
+    text = " ".join(tokens)
+    nlp = spacy.load("fr_core_news_sm")
+    doc = nlp(text)
+
+    new_tokens = [
+        token.lemma_
+        for token in doc
+    ]
+    return new_tokens
+
+def tokenization(text:str)->str:
+    return re.findall(r"\w+",text.lower().strip())
 
 if __name__ == "__main__":
-    preproces_text("Les chats mangent vite.")
+    preprocess_text("Les chats mangent vite.")
